ece0e6c08b014e79a83b1e6fcbdefcbdb924d865
Bacari Hersey <herseyb71@gmail.com>
2021-04-14 23:01:06 -0500

    Initial code for the product Api endpoints

---
+++ src/test/java/com/NWACoders/mkOne/MkOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NWACoders.mkOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
